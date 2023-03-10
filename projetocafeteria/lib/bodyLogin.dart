7630933d9f7ba6ee4ed810087971c721dd21f556
Jeferson <jefersonz.valgoi@gmail.com>
2023-03-10 01:01:45 -0300

    continuando tela login

--- projetocafeteria/lib/bodyLogin.dart
+++ projetocafeteria/lib/bodyLogin.dart
@@ -1,33 +1,101 @@
-import 'package:flutter/material.dart';
+import 'dart:html';
 
-class BodyLogin extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:projetocafeteria/cadastro.dart';
+
+import 'homePage.dart';
+
+class BodyLogin extends StatefulWidget {
   const BodyLogin({super.key});
 
   @override
-  Widget build(BuildContext context) => Container(
-    color: const Color.fromARGB(255, 34, 0, 16),
-    alignment: Alignment.center,
-    child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        TextFormField(
-          decoration: const InputDecoration(
-            labelText: "Email", border: OutlineInputBorder() 
+  State<BodyLogin> createState() => _BodyLoginState();
+}
+
+class _BodyLoginState extends State<BodyLogin> {
+
+  String email = '';
+  String senha = '';
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+    
+    body: Container(
+      padding: const EdgeInsets.all(20.0),
+      color: const Color.fromARGB(255,36, 1, 22),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SizedBox(
+            height: 400,
+            width: 400,
+            child: Image.asset('assets/img/case.png')
           ),
-        ),
-        const SizedBox(height: 20),
-        TextFormField(
-          decoration: const InputDecoration(
-            labelText: "Senha", border: OutlineInputBorder() 
+          const Text("Caso escolha um café, pare aqui!"),
+          const SizedBox(height: 20,),
+          Card(
+            child: Padding(
+              padding: const EdgeInsets.all(12.0),
+                child: Column(
+                  children: [
+                    TextField(
+                      onChanged: (txt) {
+                      email = txt;
+                      },
+                      decoration: const InputDecoration(
+                        labelText: "Email", 
+                        border: OutlineInputBorder() 
+                      ),
+                      keyboardType: TextInputType.emailAddress,
+                  
+                    ),
+                    const SizedBox(height: 10),
+                    TextField(
+                      onChanged: (txt) {
+                       senha = txt;
+                      },
+                      style: const TextStyle(color: Colors.black),
+                      decoration: const InputDecoration(
+                        labelText: "Senha", 
+                        border: OutlineInputBorder(),
+                        hintStyle: TextStyle(color: Colors.black),
+                        
+                        
+                      ),
+                      obscureText: true,
+                    ),
+                    const SizedBox(height: 20),
+                    SizedBox(
+                      width: double.infinity,
+                      child: TextButton(
+                        style: TextButton.styleFrom(
+                          primary: Colors.white,
+                          backgroundColor: Color.fromARGB(217,244,90,36),
+                          elevation: 50,
+                          shadowColor: Colors.black
+                        ),
+                        onPressed: (){
+                          Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => HomePage()));
+                        }, 
+                        child: const Text("Entrar",
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        )
+                      )
+              
+                    ),
+                  ],
+                ),
+            ),
           ),
-        ),
-        const SizedBox(height: 20),
-        ElevatedButton(
-          onPressed: (){}, 
-          child: const Text("Login")
-        )
-      ],
+          const SizedBox(height: 20,),
+          GestureDetector(
+            child: const Text("Ainda não é cadastrado? Cadastre-se aqui!"),
+            onTap: (){
+              Navigator.of(context).push(MaterialPageRoute(builder: (context) => Cadastro()));
+            },
+          )
+        ],
+      ),
     ),
   );
-  
 }

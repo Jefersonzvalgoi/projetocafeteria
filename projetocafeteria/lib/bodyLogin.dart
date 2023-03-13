3082ed2b9bfa450c7c538b63f17daff72772a6e3
Jeferson <jefersonz.valgoi@gmail.com>
2023-03-13 18:41:02 -0300

    shape botao

--- projetocafeteria/lib/bodyLogin.dart
+++ projetocafeteria/lib/bodyLogin.dart
@@ -35,50 +35,56 @@ class _BodyLoginState extends State<BodyLogin> {
           const Text("Caso escolha um café, pare aqui!"),
           const SizedBox(height: 20,),
           Card(
-            child: Padding(
-              padding: const EdgeInsets.all(12.0),
-                child: Column(
-                  children: [
-                    TextField_app(
-                      onChanged: (txt){
-                        email = txt;
-                      },
-                      obscureText: false,
-                      labelText: "Email",
-                      keyboardType: TextInputType.emailAddress,
-                    ),
-                    const SizedBox(height: 10),
-                    TextField_app(
-                      onChanged: (txt) {
-                       senha = txt;
-                      }, 
-                      obscureText: true, 
-                      labelText: "Senha",
-                      keyboardType: TextInputType.none,
-                    ),
-                    const SizedBox(height: 20),
-                    SizedBox(
-                      width: double.infinity,
-                      child: TextButton(
-                        
-                        style: TextButton.styleFrom(
-                          
-                          primary: Colors.white,
-                          backgroundColor: Color.fromARGB(217,244,90,36),
-                          elevation: 50,
-                          shadowColor: Colors.black
-                        ),
-                        onPressed: (){
-                          Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => HomePage()));
+            child: Container(
+              color: Colors.grey,
+              child: Padding(
+                padding: const EdgeInsets.all(12.0),
+                  child: Column(
+                    children: [
+                      TextField_app(
+                        onChanged: (txt){ 
+                          email = txt;
+                        },
+                        obscureText: false,
+                        labelText: "Email",
+                        keyboardType: TextInputType.emailAddress,
+                      ),
+                      const SizedBox(height: 10),
+                      TextField_app(
+                        onChanged: (txt) {
+                         senha = txt;
                         }, 
-                        child: const Text("Entrar",
-                          style: TextStyle(fontWeight: FontWeight.bold),
+                        obscureText: true, 
+                        labelText: "Senha",
+                        keyboardType: TextInputType.none,
+                      ),
+                      const SizedBox(height: 20),
+                      SizedBox(
+                        width: double.infinity,
+                        child: TextButton(
+                          
+                          style: TextButton.styleFrom(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20.00)
+                            ),
+                            
+                            primary: Colors.white,
+                            backgroundColor:const Color.fromARGB(217,244,90,36),
+                            elevation: 50.00,
+                            shadowColor: Colors.black
+                          ),
+                          onPressed: (){
+                            Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => HomePage()));
+                          }, 
+                          child: const Text("Entrar",
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          )
                         )
-                      )
-              
-                    ),
-                  ],
-                ),
+                
+                      ),
+                    ],
+                  ),
+              ),
             ),
           ),
           const SizedBox(height: 20,),
